39ddf7d3d76621ad38cb0d68006a8a6f5d8bc745
Joshua Wingstrom <josh@eastfive.com>
2020-02-11 19:37:55 -0600

    Setup project w/ submodules and have functions, authentication, and storage all added

---
+++ EastFive.Admin.UI/Comms/Resources/StorageTable.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EastFive.Azure.Storage
+{
+    public class StorageTable
+    {
+        public string name;
+
+        public StorageProperty[] properties;
+    }
+
+    public class StorageProperty
+    {
+        public string name;
+
+        public string type;
+    }
+
+    public class StorageRow
+    {
+        #region Properties
+
+        [JsonProperty]
+        public string rowKey;
+
+        [JsonProperty]
+        public string partitionKey;
+
+        [JsonProperty]
+        public IDictionary<string, object> properties;
+
+        #endregion
+    }
+}
